25e3c76abe0e66b0a4588db6cbf1e37003ef05c2
Sahil_Pop! <sahil_pop@pop-os.localdomain>
2024-07-31 19:14:24 +0530

    second

--- src/main/java/com/amdocs/Final/FProApplication.java
+++ src/main/java/com/amdocs/Final/FProApplication.java
@@ -1,5 +1,7 @@
 package com.amdocs.Final;
 
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,61 @@ public class FProApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FProApplication.class, args);
+		
+
+		
+//		Lets objectify xD
+		Scanner sc = new Scanner(System.in);
+		Store vary = new Store();
+		
+//-----------------------INPUT-START--------------------
+		
+		// Input Weight
+		
+		System.out.println("Please Enter Your Weight in Kgs (Float)");
+		
+		vary.weight = sc.nextFloat();
+		
+		// Input Age
+		
+	System.out.println("Please Enter Your Age (Int)");
+		
+		vary.age = sc.nextInt();
+		
+		
+		// Input Height
+	System.out.println("Please Enter Your Height in Meters (Float)");
+		
+		vary.height = sc.nextFloat();
+		
+		// Input sex
+		
+	System.out.println("Please Enter Your Sex (F for Female $$	T for Rest)");
+		
+		vary.sex = sc.nextBoolean();
+		
+//	--------------------INPUT-END------------------------
+
+		
+//		--------------------Genius-Start------------------------
+		
+		vary.bmi = vary.weight/(vary.height*vary.height);
+		
+		
+		
+//		--------------------Genius-END------------------------
+		
+		
+		
+//		--------------------Output-START------------------------
+		System.out.println("You Entered the following Details" +"\n" + "Age: " + vary.age +"\n" + "Height: " +   vary.height +"\n" + "Weight: " +   vary.weight +"\n" + "Sex: " +  vary.sex  );
+
+		System.out.println("Yor BMI is :" + vary.bmi );
+		
+		
+		//		--------------------Output-END------------------------
+		
+	
 	}
 
 }
